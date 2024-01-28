2968db7dd49f8eadc1186d84ddf501e4eb623e1a
Gh-Yeh <yehyaghady@gmail.com>
2024-01-28 03:14:26 -0800

    pythonfiles

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -3,6 +3,11 @@ def print_matrix_integer(matrix=[[]]):
     if not matrix:
         print()
     else:
-        for mtrx in matrix:
-            for row in mtrx:
-                print("{:d}".format(row), end=" " if row != mtrx[-1] else "\n")
+        for row in range(len(matrix)):
+            for item in range(len(matrix[row])):
+                if item != len(matrix[row]) - 1:
+                    endspace = ' '
+                else:
+                    endspace = ''
+                print("{:d}".format(matrix[row][item]), end=endspace)
+            print()
