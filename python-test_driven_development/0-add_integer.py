67e9a97d103a84cc29e304030e55d2e5f528c073
Gh-Yeh <yehyaghady@gmail.com>
2024-02-08 19:22:07 -0800

    pythonfiles

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -9,8 +9,8 @@ def add_integer(a, b=98):
     Returns:
         An integer of the sum between a and b.
     """
-    if type(a) not in [int, float]:
+    if ((not isinstance(a, int) and not isinstance(a, float))):
         raise TypeError("a must be an integer")
-    if type(b) not in [int, float]:
+    if ((not isinstance(b, int) and not isinstance(b, float))):
         raise TypeError("b must be an integer")
-    return int(a) + int(b)
+    return (int(a) + int(b))
