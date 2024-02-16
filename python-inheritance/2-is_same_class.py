da162e1574b24390989edee3b7d2176f7ac14063
Gh-Yeh <yehyaghady@gmail.com>
2024-02-15 18:51:43 -0800

    pythonfiles

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -1,17 +1,12 @@
 #!/usr/bin/python3
-'''Module for is_same_class method.'''
+"""
+================================
+module with method is_same_class
+================================
+"""
 
 
 def is_same_class(obj, a_class):
-    """Check if an object is exactly an instance of a given class.
+    """Method that return True if an object is an instance of a class"""
 
-    Args:
-        obj (any): The object to check.
-        a_class (type): The matching class
-    Returns:
-        If obj is exactly an instance of a_class - True.
-        Otherwise - False.
-    """
-    if type(obj) == a_class:
-        return True
-    return False
+    return type(obj) is a_class
--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -1,25 +1,22 @@
 #!/usr/bin/python3
-'''Module for BaseGeometry class.'''
+"""
+===================================
+module with class BaseGeometry
+===================================
+"""
 
 
 class BaseGeometry:
-    '''A BaseGeometry class.'''
+    """BaseGeometry class"""
 
     def area(self):
-        '''Method to calculate this area.'''
-        raise Exception('area() is not implemented')
+        """method for calculated area"""
+        raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
-        """Validating parameter.
+        """Method for validate if a num is integer"""
 
-        Args:
-            name (str): The name of the parameter.
-            value (int): The parameter to validate.
-        Raises:
-            TypeError: If value is not an integer.
-            ValueError: If value is <= 0.
-        """
-        if type(value) != int:
+        if type(value) is not int:
             raise TypeError("{} must be an integer".format(name))
         if value <= 0:
             raise ValueError("{} must be greater than 0".format(name))
