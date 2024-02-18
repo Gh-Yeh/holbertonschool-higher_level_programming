62581c21420a1204edd2a845410d2b571efafad7
Gh-Yeh <yehyaghady@gmail.com>
2024-02-18 07:55:41 -0800

    pythonfiles

--- python-input_output/10-student.py
+++ python-input_output/10-student.py
@@ -1,32 +1,30 @@
 #!/usr/bin/python3
-"""Defines a class Student."""
+"""
+    Defines a student class
+"""
 
 
 class Student:
-    """Represent a student."""
+    """
+        Defines a student class
+    """
 
     def __init__(self, first_name, last_name, age):
-        """Initialize a new Student.
-
-        Args:
-            first_name (str): The first name of the student.
-            last_name (str): The last name of the student.
-            age (int): The age of the student.
+        """
+            Initializing instance variables
         """
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self, attrs=None):
-        """Get a dictionary representation of the Student.
-
-        If attrs is a list of strings, represents only those attributes
-        included in the list.
-
-        Args:
-            attrs (list): (Optional) The attributes to represent.
         """
-        if (type(attrs) == list and
-                all(type(ele) == str for ele in attrs)):
-            return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
-        return self.__dict__
+            Retrieves the dict reprenting the class
+        """
+        if attrs is None:
+            return self.__dict__
+        my_dict = {}
+        for attr in attrs:
+            if attr in self.__dict__.keys():
+                my_dict[attr] = self.__dict__[attr]
+        return my_dict
--- python-input_output/11-student.py
+++ python-input_output/11-student.py
@@ -1,41 +1,35 @@
 #!/usr/bin/python3
-"""Defines a class Student."""
+"""Defines a student class
+"""
 
 
 class Student:
-    """Represent a student."""
+    """Defines a student class"""
 
     def __init__(self, first_name, last_name, age):
-        """Initialize a new Student.
+        """Initializes Student"""
 
-        Args:
-            first_name (str): The first name of the student.
-            last_name (str): The last name of the student.
-            age (int): The age of the student.
-        """
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self, attrs=None):
-        """Get a dictionary representation of the Student.
+        """Retrieves a dictionary representation of a Student instance"""
 
-        If attrs is a list of strings, represents only those attributes
-        included in the list.
-
-        Args:
-            attrs (list): (Optional) The attributes to represent.
-        """
-        if (type(attrs) == list and
-                all(type(ele) == str for ele in attrs)):
-            return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
-        return self.__dict__
+        if attrs is None:
+            return self.__dict__
+        my_dict = {}
+        for attr in attrs:
+            if attr in self.__dict__.keys():
+                my_dict[attr] = self.__dict__[attr]
+        return my_dict
 
     def reload_from_json(self, json):
-        """Replace all attributes of the Student.
+        """Replaces all attributes of the class"""
 
-        Args:
-            json (dict): The key/value pairs to replace attributes with.
-        """
-        for k, v in json.items():
-            setattr(self, k, v)
+        current_dict = self.__dict__
+        for attr in json.keys():
+            for current_attr in current_dict.keys():
+                if attr == current_attr:
+                    current_dict[current_attr] = json[attr]
+        return current_dict
