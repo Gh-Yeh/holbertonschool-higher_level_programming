28dbc632e5bdd5201b446a56076a5b858eb2579b
Gh-Yeh <yehyaghady@gmail.com>
2024-02-18 07:21:08 -0800

    pythonfiles

--- python-input_output/11-student.py
+++ python-input_output/11-student.py
@@ -16,3 +16,9 @@ class Student:
         if (not isinstance(attrs, list) or attrs is None):
             return self.__dict__
         return {item: self.__dict__[item] for item in attrs if item in self.__dict__}
+
+    def reload_from_json(self, json):
+        """Replace all attributes of the Student."""
+
+        for name, value in json.items():
+            setattr(self, name, value)
