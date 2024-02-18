7c5e95377e7870b53f1c62397bbb983258716da5
Gh-Yeh <yehyaghady@gmail.com>
2024-02-18 07:50:09 -0800

    pythonfiles

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -1,15 +1,16 @@
 #!/usr/bin/python3
-"""Defines a file-writing function and returns the number of characters added."""
+"""Contains function that writes a
+string to a text file (UTF8)
+and returns the number of character
+"""
 
 
 def write_file(filename="", text=""):
-    """Write a string to the file.
-
-    Args:
-        filename (str): The name of the file to write.
-        text (str): The text to write to the file.
-    Returns:
-        The number of characters written.
     """
-    with open(filename, "w", encoding="utf-8") as f:
-        return f.write(text)
+    Write a string to a text file (UTF-8) and return the
+    number of characters written.
+    """
+    with open(filename, 'w', encoding='utf-8') as file:
+        count = file.write(text)
+
+    return count
