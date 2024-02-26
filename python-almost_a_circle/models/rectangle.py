28b67eb2ae156ab65fd80058b32d7ef0dd6e4648
Gh-Yeh <yehyaghady@gmail.com>
2024-02-26 05:29:07 +0000

    tasks

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -20,11 +20,11 @@ class Rectangle(Base):
             TypeError: If either of x or y is not an int.
             ValueError: If either of x or y < 0.
         """
+        super().__init__(id)
         self.__width = width
         self.__x = x
         self.__height = height
         self.__y = y
-        super().__init__(id)
 
     @property
     def width(self):
@@ -48,6 +48,7 @@ class Rectangle(Base):
 
     @width.setter
     def width(self, value):
+        '''setter method for the attribute width'''
         if type(value) != int:
             raise TypeError("width must be an integer")
         if value <= 0:
@@ -56,6 +57,7 @@ class Rectangle(Base):
 
     @height.setter
     def height(self, value):
+        '''setter method for the attribute height'''
         if type(value) != int:
             raise TypeError("height must be an integer")
         if value <= 0:
@@ -64,6 +66,7 @@ class Rectangle(Base):
 
     @x.setter
     def x(self, value):
+        '''setter method for the attribute x'''
         if type(value) != int:
             raise TypeError("x must be an integer")
         if value < 0:
@@ -72,6 +75,7 @@ class Rectangle(Base):
 
     @y.setter
     def y(self, value):
+        '''setter method for the attribute y'''
         if type(value) != int:
             raise TypeError("y must be an integer")
         if value < 0:
