c38335a60ea730c388c18753f5f029c004048e58
Gh-Yeh <yehyaghady@gmail.com>
2024-03-31 21:38:17 +0300

    tasks

--- SQL_more_queries/5-unique_id.sql
+++ SQL_more_queries/5-unique_id.sql
@@ -1,3 +1,5 @@
--- creates the table force_name on your MySQL server
--- creates a table in a database
-CREATE TABLE IF NOT EXISTS force_name (id INT, name VARCHAR(256) NOT NULL);
+-- creates the table unique_id on mysql server
+-- database name passed as an argument of the mysql command
+CREATE TABLE IF NOT EXISTS unique_id (id INT DEFAULT 1 UNIQUE,
+		name VARCHAR(256)
+		);
